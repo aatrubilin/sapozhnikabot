fa98035b0a31bf40e1eaa13b5d048e4649f3a555
AATrubilin <aatrubilin@gmail.com>
2021-08-07 02:06:27 +0700

    ❗ add error handler

--- sapozhnikabot/main.py
+++ sapozhnikabot/main.py
@@ -1,10 +1,13 @@
+import html
+import json
 import logging
 import os
+import traceback
 
 import rhymer
 import utils
 from db import Database
-from telegram import Bot, Update
+from telegram import Bot, ParseMode, Update
 from telegram.ext import CallbackContext, ContextTypes, Filters, MessageHandler, Updater
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
@@ -12,6 +15,9 @@ TIMEOUT_SEC = int(os.environ.get("TIMEOUT_SEC", 7200))
 SWEARS_DATASET_PATH = os.environ.get("SWEARS_DATASET_PATH", "")
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
 SQLITE_DB_PATH = os.environ.get("SQLITE_DB_PATH", "db.sqlite")
+DEVELOPER_CHAT_IDS = os.environ.get("DEVELOPER_CHAT_IDS", "")
+if DEVELOPER_CHAT_IDS:
+    DEVELOPER_CHAT_IDS = [int(chat_id_) for chat_id_ in DEVELOPER_CHAT_IDS.split(",")]
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=LOG_LEVEL
@@ -72,6 +78,39 @@ def reply_handler(update: Update, context: CustomCallbackContext) -> None:
             logging.debug("Reply message is empty")
 
 
+def error_handler(update: object, context: CallbackContext) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logging.error(msg="Exception while handling an update:", exc_info=context.error)
+    if not DEVELOPER_CHAT_IDS:
+        return
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f"An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    # Finally, send the message
+    for chat_id in DEVELOPER_CHAT_IDS:
+        context.bot.send_message(
+            chat_id=chat_id, text=message, parse_mode=ParseMode.HTML
+        )
+
+
 if __name__ == "__main__":
     bot = Bot(token=BOT_TOKEN)
     bot_info = bot.get_me()
@@ -87,6 +126,9 @@ if __name__ == "__main__":
 
     # Reply the message on Telegram only for groups
     dispatcher.add_handler(MessageHandler(Filters.chat_type.groups, reply_handler))
+
+    # Error handler
+    dispatcher.add_error_handler(error_handler)
 
     # Start the Bot
     updater.start_polling()
